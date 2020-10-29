465dcae172ab453c1d125d6f1c95118720b9c681
TJ Janus <TJJanus1@gmail.com>
2020-10-29 13:00:57 -0400

    wip

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
+require('dotenv').config()
+
 const server = require("./api/server.js");
 
-const port = 5000;
+const port = process.env.PORT || 4000;  // On Local port will be 5000, On Heroku it will use whatever Heroku supplies
+const secret = process.env.SECRET_THING || 'foo';
+
+console.log(port, secret);
+
+process.env.PORT; // ONLY HEROKU
+
 server.listen(port, () => {
   console.log(`\n*** Server Running on http://localhost:${port} ***\n`);
 });
